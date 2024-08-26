87b97f1cae8e2d170b160532b37dc5ab26c662f7
Romuald Wandji <34631595+romy651@users.noreply.github.com>
2024-08-26 13:10:03 +0200

    Fixed the issue on the 'related' variable

--- data/process_data.py
+++ data/process_data.py
@@ -42,6 +42,7 @@ def clean_data(df):
     df = df.drop("categories", axis=1)
     df = pd.concat([df, categories], axis=1)
     df = df.drop_duplicates()
+    df['related'] = df['related'].map(lambda x: 1 if x==2 else x)
 
     return df
 
